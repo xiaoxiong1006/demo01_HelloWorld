d9476a71dea8158c1e56fc3482c6cf1f12b48ed4
xiaoxiong1006 <758063761@qq.com>
2020-02-13 17:03:09 +0800

    Feature: add a click handler to display a toast

--- app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
+++ app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.demo01_helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var iconPlay:ImageView = findViewById(R.id.icon_play)
+        iconPlay.setOnClickListener {
+            Toast.makeText(MainActivity@this,"Hello World!",Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
+
 }
+
+
+
+
