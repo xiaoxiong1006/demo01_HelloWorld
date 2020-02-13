a3d40a107d9fe56491d18819ad71e22e425db313
xiaoxiong1006 <758063761@qq.com>
2020-02-13 10:52:50 +0800

    Feature: get a reference to the play icon in code

--- app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
+++ app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.demo01_helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var iconPlay:ImageView = findViewById(R.id.icon_play)
     }
 }
