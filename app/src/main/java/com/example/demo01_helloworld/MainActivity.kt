d74f290c50ceeeb6bb3293734530b86e72722a01
xiaoxiong1006 <758063761@qq.com>
2020-02-13 22:15:09 +0800

    Feature: add play function to the play icon

--- app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
+++ app/src/main/java/com/example/demo01_helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.demo01_helloworld
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -7,20 +8,20 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mPlayer:MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var iconPlay:ImageView = findViewById(R.id.icon_play)
+        mPlayer = MediaPlayer.create(this,R.raw.hello_world)
         iconPlay.setOnClickListener {
-            Toast.makeText(MainActivity@this,"Hello World!",Toast.LENGTH_SHORT).show()
+            mPlayer.start()
         }
     }
-
-
-
-
 }
+
 
 
 
